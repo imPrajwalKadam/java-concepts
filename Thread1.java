bf4ef6df29bca33f317216b139fa267b6f0d8ec5
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-08-12 14:41:44 +0530

    Add files via upload

---
+++ Thread1.java
@@ -0,0 +1,8 @@
+class Thread1
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Inside main thread");
+        System.out.println(Thread.currentThread().getName());
+    }
+}
